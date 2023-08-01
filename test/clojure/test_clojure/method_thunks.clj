efb72071b21988847aea2a266de16c4920d1adaa
fogus <mefogus@gmail.com>
2023-08-01 10:41:09 -0400

    spacing

--- test/clojure/test_clojure/method_thunks.clj
+++ test/clojure/test_clojure/method_thunks.clj
@@ -5,7 +5,6 @@
 ;   By using this software in any fashion, you are agreeing to be bound by
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
-
 ; Authors: Fogus
 
 (ns clojure.test-clojure.method-thunks
